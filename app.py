346b5a93381b204bbab3dca2c7f435f8788b87e7
Débora Eduarda <deboraeduarda@macbookpro.mynetworksettings.com>
2025-08-09 20:01:00 -0400

    fix app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
-
-# Ler o arquivo CSV
-car_data = pd.read_csv('vehicles_us.csv')
-
-# Cabeçalho
-st.header('Análise de anúncios de carros')
-
-# Botão para criar histograma
-hist_button = st.button('Criar histograma')
-
-if hist_button:
-    st.write('Criando um histograma para a coluna "odometer"')
-    fig = px.histogram(car_data, x='odometer')
-    st.plotly_chart(fig, use_container_width=True)
-
-# Botão para criar gráfico de dispersão
-scatter_button = st.button('Criar gráfico de dispersão')
-
-if scatter_button:
-    st.write('Criando gráfico de dispersão entre "price" e "odometer"')
-    fig = px.scatter(car_data, x='odometer', y='price')
-    st.plotly_chart(fig, use_container_width=True)
+import streamlit as st
+        
+car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+        
+if hist_button: # se o botão for clicado
+# escrever uma mensagem
+st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+            
+# criar um histograma
+fig = px.histogram(car_data, x="odometer")
+        
+# exibir um gráfico Plotly interativo
+st.plotly_chart(fig, use_container_width=True)
